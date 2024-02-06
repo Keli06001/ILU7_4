c17b69b8800ce9b2876ff75eb27a71cb570a3ed4
Keli06001 <keli06001@utb.vaxjo.se>
2024-02-06 10:31:17 +0100

    Add project files.

---
+++ ILU7_4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace ILU7_4;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] strings = { "hej", "nej", "ja" };
+
+        char characterToFind1 = 'j';
+        Console.WriteLine($"Finns '{characterToFind1}' i alla strängar? {FinnsTecknetIAllaSträngar(strings, characterToFind1)}");
+
+        char characterToFind2 = 'e';
+        Console.WriteLine($"Finns '{characterToFind2}' i alla strängar? {FinnsTecknetIAllaSträngar(strings, characterToFind2)}");
+    }
+
+    static bool FinnsTecknetIAllaSträngar(string[] strings, char character)
+    {
+        foreach (string str in strings)
+        {
+            if (!str.Contains(character))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
